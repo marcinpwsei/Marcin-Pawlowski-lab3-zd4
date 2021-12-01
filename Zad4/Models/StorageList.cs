9ae3854a1ce3b725d6a1940082b357ed78b6f109
Marcin Pawlowski <marcin.pawlowski@microsoft.wsei.edu.pl>
2021-12-01 19:57:03 +0100

    feat: add model for storing items

---
+++ Zad4/Models/StorageList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Zad4.Models
+{
+    public class StorageList
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        [DataType(DataType.Date)] //we need Date only
+        public DateTime ValidToDate { get; set; }
+        public decimal Weight { get; set; }
+        public string LockerName { get; set; }
+
+    }
+}
